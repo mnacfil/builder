b2d8745b2afd771e9f8489e29d59121fae19ed41
mnacfil <mnacfil@gmail.com>
2024-05-27 21:15:51 +0800

    fix error when login in, install other shadcn components, install mysql server

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/(main)(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/(main)/(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
