c230f1f2ecf9ad8b08cb01170d4ad2f78e9ca978
mnacfil <mnacfil@gmail.com>
2024-07-15 19:48:22 +0800

    add subpages in agency

--- src/components/global/infobar.tsx
+++ src/components/global/infobar.tsx
@@ -17,6 +17,7 @@ import { Role } from "@prisma/client";
 import { Card } from "../ui/card";
 import { Switch } from "../ui/switch";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { ModeToggle } from "./mode-toggle";
 
 type Props = {
   notifications: NotificationWithUser | [];
@@ -52,9 +53,10 @@ const Infobar = ({ notifications, subAccountId, className, role }: Props) => {
     >
       <div className="flex items-center gap-2 ml-auto">
         <UserButton />
+
         <Sheet>
           <SheetTrigger>
-            <div className="rounded-full w-8 h-8 bg-primary flex items-center justify-center text-white">
+            <div className="rounded-full w-9 h-9 bg-primary flex items-center justify-center text-white">
               <Bell size={17} />
             </div>
           </SheetTrigger>
@@ -111,6 +113,7 @@ const Infobar = ({ notifications, subAccountId, className, role }: Props) => {
             )}
           </SheetContent>
         </Sheet>
+        <ModeToggle />
       </div>
     </div>
   );
