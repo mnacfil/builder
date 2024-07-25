b59cde23ee9380edec31d2657e5593d4db58e88a
mnacfil <mnacfil@gmail.com>
2024-07-25 21:33:19 +0800

    create all subaccounts page

---
+++ src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SubAccountIdPage = (props: Props) => {
+  return <div>Subaccount id</div>;
+};
+
+export default SubAccountIdPage;
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -429,3 +429,19 @@ export const changeUserPermissions = async (
   });
   return response;
 };
+
+export const getSubaccountDetails = async (subaccountId: string) => {
+  const response = await db.subAccount.findUnique({
+    where: { id: subaccountId },
+  });
+
+  return response;
+};
+
+export const deleteSubAccount = async (subaccountId: string) => {
+  const response = await db.subAccount.delete({
+    where: { id: subaccountId },
+  });
+
+  return response;
+};
